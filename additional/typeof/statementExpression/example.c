be1ede0eb004eb56e9b28e86fff826e85d0294b9
chaotic-Coder2002 <cpr0gramm3r@yahoo.com>
2025-10-31 19:35:55 +0530

    an example

---
+++ additional/typeof/statementExpression/example.c
@@ -0,0 +1,31 @@
+/*!
+  \file example.c
+
+  This program demonstrates how the macro \ref "maxInt(a, b)" does <em>variable shadowing</em>
+
+  Variable shadowing occurs when a variable declared in an inner scope takes precedence over a variable
+  with the same name declared in an outer scope.
+
+  Look at line 14. If we expand the call to the macro at that line, then this is what we get:
+
+  \code{.c}
+  int c = ({
+  int _a = _a; // _a is actually the local variable here
+  int _b = _b; // same with _b, hence both initially store garbage value
+  _a > _b ? _a : _b; // so, garbage is returned. GIGO
+  });
+  \endcode
+ */
+
+#include "defs.h"
+#include<stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  int _a = 1, _b = 2;
+
+  int c = maxInt(_a, _b);
+
+  printf("Max: %d\n", c);
+  return 0;
+}
