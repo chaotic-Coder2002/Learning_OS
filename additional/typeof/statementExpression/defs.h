8bd7b55ac6d73df4d7a55c9cc6db4156d9529b7a
chaotic-Coder2002 <cpr0gramm3r@yahoo.com>
2025-10-31 19:35:46 +0530

    statement expressions

--- additional/typeof/statementExpression/defs.h
+++ additional/typeof/statementExpression/defs.h
@@ -9,14 +9,19 @@
 /*!
   \def maxInt(a, b)
   \brief Again, finds the larger among \a a and \a b.
-  This is a modification of \ref "max(a, b)". Here, (a) and (b) are evaluated only once. Their values are
+
+  \details This is a modification of \ref "max(a, b)". Here, (a) and (b) are evaluated only once. Their values are
   assigned to the local variables \a _a and \a _b. Note that since this is actually statement expression,
-  therefore, the last statement must always be an expression. So, it is.
+  therefore, the last statement must always be an expression.
+
+  But this function has a problem and that is <em>variable shadowing</em>.
+  \example example.c
  */
 
 /*!
   \brief Finds the larger one among \a a and \a b.
-  However this has one problem and that is, \a (a) or \a (b) is evaluated twice. Once
+
+  \details However this has one problem and that is, \a (a) or \a (b) is evaluated twice. Once
   when they are being compared and once depending on whether the conditional evaluates to true or false.
   \param a An integer value
   \param b Another integer value
@@ -28,7 +33,7 @@
   ({									\
     int _a = (a);							\
     int _b = (b);							\
-    _a > _b ? _a : _b;/*!< this is like a return statement */		\
+    _a > _b ? _a : _b;							\
   })
 
 #endif
